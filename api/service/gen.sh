58aa114e43c39c2a08445ee1638a62a0541dfcce
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2021-04-02 05:18:38 +0800

    Feat: update protobuf & new generation script

--- api/service/gen.sh
+++ api/service/gen.sh
@@ -1,1 +1,27 @@
-protoc ./api.proto --go_out=plugins=grpc:.
+#!/usr/bin/env bash
+
+echo "Processing..."
+
+GOPATH=${GOPATH:-$(go env GOPATH)}
+GOBIN=${GOBIN:-$(go env GOBIN)}
+
+if [[ $GOBIN == "" ]]; then
+  GOBIN=${GOPATH}/bin
+fi
+
+go install -v google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+echo "Use protoc-gen-go and protoc-gen-go-grpc in $GOBIN."
+
+protoc --go_out=. \
+--go_opt=paths=source_relative \
+--go-grpc_out=. \
+--go-grpc_opt=paths=source_relative \
+--plugin=protoc-gen-go=${GOBIN}/protoc-gen-go \
+--plugin=protoc-gen-go-grpc=${GOBIN}/protoc-gen-go-grpc \
+api.proto
+
+if [ $? -eq 0 ]; then
+  echo "Generated successfully."
+fi
